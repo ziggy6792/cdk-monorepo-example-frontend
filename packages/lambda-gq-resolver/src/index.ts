44ae243431035dc041703279dcd883307607753b
Simon <simonaverhoeven@gmail.com>
2020-12-02 11:19:17 +0800

    updated lambda

--- packages/lambda-gq-resolver/src/index.ts
+++ packages/lambda-gq-resolver/src/index.ts
@@ -1,8 +1,29 @@
 /* eslint-disable import/prefer-default-export */
 import 'source-map-support/register';
+import {ApolloServer} from 'apollo-server-express'
+import { buildSchema, Resolver } from 'type-graphql';
+
+@Resolver()
+class RecipeResolver {
+  private recipesCollection: Recipe[] = [];
+
+  async recipes() {
+    // fake async in this example
+    return await this.recipesCollection;
+  }
+}
 
 export const handler = async (event: any): Promise<any> => {
-  return {
+
+  const shcema = await buildSchema (
+    resolvers: [FirstResolver, SampleResolver],
+  )
+
+  const apolloServer = new ApolloServer({})
+
+
+
+  return { 
     statusCode: 200,
     body: 'hello from lambda-gq-resolver',
   };
