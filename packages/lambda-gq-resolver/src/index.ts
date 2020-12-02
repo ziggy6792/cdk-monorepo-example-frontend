e0a1b62cedcbe0fcb3e43b21d4b56bd5bc6334b8
Simon <simonaverhoeven@gmail.com>
2020-12-02 15:58:22 +0800

    using aws-serverless-express

--- packages/lambda-gq-resolver/src/index.ts
+++ packages/lambda-gq-resolver/src/index.ts
@@ -1,47 +1,19 @@
-// /* eslint-disable class-methods-use-this */
-// /* eslint-disable import/prefer-default-export */
-// import 'reflect-metadata';
-// import { ApolloServer } from 'apollo-server-lambda';
-// import { buildSchema, Resolver, Query } from 'type-graphql';
-
-// @Resolver()
-// class HelloResolver {
-//   private recipesCollection: string[] = [];
-
-//   @Query((returns) => [String])
-//   async hello() {
-//     return 'Hello World';
-//   }
-// }
-
-// const globalSchema = buildSchema({
-//   resolvers: [HelloResolver],
-// });
-
-// async function getServer() {
-//   const schema = await globalSchema;
-//   return new ApolloServer({
-//     schema,
-//   });
-// }
-
-// export function handler(event: any, ctx: any, callback: any) {
-//   getServer()
-//     .then((server) => server.createHandler())
-//     .then((handler) => handler(event, ctx, callback));
-// }
-
-/* eslint-disable class-methods-use-this */
 /* eslint-disable import/prefer-default-export */
+/* eslint-disable class-methods-use-this */
+// import { graphiqlExpress, graphqlExpress } from 'apollo-server-express';
+
 import 'reflect-metadata';
-import { ApolloServer } from 'apollo-server-lambda';
-import { buildSchema, Resolver, Query, buildSchemaSync } from 'type-graphql';
+import * as serverless from 'aws-serverless-express';
+import Express from 'express';
+import { buildSchemaSync, Resolver, Query } from 'type-graphql';
+import { ApolloServer } from 'apollo-server-express';
+import { GraphQLSchema } from 'graphql';
 
 @Resolver()
 class HelloResolver {
   private recipesCollection: string[] = [];
 
-  @Query((returns) => [String])
+  @Query((returns) => String)
   async hello() {
     return 'Hello World';
   }
@@ -53,6 +25,15 @@ class HelloResolver {
     resolvers: [HelloResolver],
   });
 
-const server = new ApolloServer({ schema: (global as any).schema });
+const schema = (global as any).schema as GraphQLSchema;
 
-exports.handler = server.createHandler();
+export const handler = (event, context) => {
+  const app = Express();
+
+  const apolloServer = new ApolloServer({ schema, playground: true, introspection: true });
+
+  apolloServer.applyMiddleware({ app });
+
+  const server = serverless.createServer(app);
+  return serverless.proxy(server, event, context);
+};
