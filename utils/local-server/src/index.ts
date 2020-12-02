b616551e62dd9be331aaf03348761e3af320eebb
Simon <simonaverhoeven@gmail.com>
2020-12-02 17:28:09 +0800

    workspace for local server

--- utils/local-server/src/index.ts
+++ utils/local-server/src/index.ts
@@ -1,18 +1,21 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import path from 'path';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
+// import schema from '@danielblignaut/lambda-gq-resolver/graph-ql/schema';
 import lambdaLocal = require('lambda-local');
 
 const app = express();
 
 // Setup local grapql server
 
+// const apolloServer = new ApolloServer({ schema });
+
 app.use(express.text());
 
 app.use('/lambda-a', async (req, res) => {
+  console.log(`path is ${path.join(__dirname, '../../../packages/lambda-a/src/index')}`);
   const result = await lambdaLocal.execute({
-    lambdaPath: path.join(__dirname, '../packages/lambda-a/src/index'),
+    lambdaPath: path.join(__dirname, '../../../packages/lambda-a/src/index'),
     lambdaHandler: 'handler',
     envfile: path.join(__dirname, '.env-lambda'),
     event: {
@@ -29,7 +32,7 @@ app.use('/lambda-a', async (req, res) => {
 
 app.use('/lambda-b', async (req, res) => {
   const result = await lambdaLocal.execute({
-    lambdaPath: path.join(__dirname, '../packages/lambda-b/src/index'),
+    lambdaPath: path.join(__dirname, '../../../packages/lambda-b/src/index'),
     lambdaHandler: 'handler',
     envfile: path.join(__dirname, '.env-lambda'),
     event: {
@@ -46,7 +49,7 @@ app.use('/lambda-b', async (req, res) => {
 
 app.use('/lambda-gq-resolver', async (req, res) => {
   const result = await lambdaLocal.execute({
-    lambdaPath: path.join(__dirname, '../packages/lambda-gq-resolver/src/index'),
+    lambdaPath: path.join(__dirname, '../../../packages/lambda-gq-resolver/src/index'),
     lambdaHandler: 'handler',
     envfile: path.join(__dirname, '.env-lambda'),
     event: {
