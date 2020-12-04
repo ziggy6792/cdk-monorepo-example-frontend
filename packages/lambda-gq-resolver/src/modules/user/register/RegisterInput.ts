962f906571304a5dffb72c9bac1370fdfb44939c
Simon <simonaverhoeven@gmail.com>
2020-12-05 01:20:02 +0800

    fixed bug with custom validators

--- packages/lambda-gq-resolver/src/modules/user/register/RegisterInput.ts
+++ packages/lambda-gq-resolver/src/modules/user/register/RegisterInput.ts
@@ -1,12 +1,13 @@
 /* eslint-disable import/prefer-default-export */
-import { MaxLength, Length, IsEmail } from 'class-validator';
+import { MaxLength, Length, IsEmail, IsOptional } from 'class-validator';
 import { Field, ID, InputType } from 'type-graphql';
+import { IsEmailAlreadyExist } from './isEmailAlreadyExist';
 import { IsIdAlreadyExist } from './isIdAlreadyExist';
 
 @InputType()
 export class RegisterInput {
+  @IsEmailAlreadyExist()
   @Field(() => ID, { nullable: true })
-  @IsIdAlreadyExist()
   id: string;
 
   @Field()
