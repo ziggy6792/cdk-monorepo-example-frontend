07531fdbfe70dab1dda8871a453723766bde8dab
Simon <simonaverhoeven@gmail.com>
2020-12-29 15:13:12 +0800

    testing eslint

--- packages/web-app/src/modules/users-list/users-list.tsx
+++ packages/web-app/src/modules/users-list/users-list.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import { useQuery } from 'react-apollo';
 import { HELLO } from '../../graphql/custom-queries';
 
-const UsersList = () => {
+const UsersList: React.FC = () => {
   const { loading, data, error } = useQuery(HELLO);
 
   if (error) {
     console.log('error', error);
   }
+
+  // const apollo = useApolloClient();
+
+  // useEffect(() => {
+  //   apollo.query({ query: HELLO });
+  // }, []);
 
   return (
     <>
