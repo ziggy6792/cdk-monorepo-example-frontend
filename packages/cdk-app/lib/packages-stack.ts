d6ef4ded5746c46fa0b836096b4769b06568d90e
Simon <simonaverhoeven@gmail.com>
2020-12-30 02:18:35 +0800

    cleanup

--- packages/cdk-app/lib/packages-stack.ts
+++ packages/cdk-app/lib/packages-stack.ts
@@ -4,6 +4,8 @@ import * as subs from '@aws-cdk/aws-sns-subscriptions';
 import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as s3Deployment from '@aws-cdk/aws-s3-deployment';
 import path from 'path';
 
 export class PackagesStack extends cdk.Stack {
@@ -16,6 +18,20 @@ export class PackagesStack extends cdk.Stack {
       runtime: lambda.Runtime.NODEJS_12_X,
       handler: 'index.handler',
       code: lambda.Code.fromAsset(path.join(require.resolve('@danielblignaut/lambda-a'), '..')),
+    });
+
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+      publicReadAccess: true,
+      websiteIndexDocument: 'index.html',
+    });
+
+    const deploylemt = new s3Deployment.BucketDeployment(this, 'DeployWebsite', {
+      destinationBucket: websiteBucket,
+      sources: [s3Deployment.Source.asset('../frontend/build')],
+    });
+
+    const output = new cdk.CfnOutput(this, 'Website Address', {
+      value: websiteBucket.bucketWebsiteUrl,
     });
   }
 }
