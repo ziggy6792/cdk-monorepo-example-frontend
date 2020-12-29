464a9cb1ab988dc3670ead2b581d7e7e4668b0ed
Simon <simonaverhoeven@gmail.com>
2020-12-29 14:10:09 +0800

    cleanup

--- packages/web-app/src/util/aws-config.ts
+++ packages/web-app/src/util/aws-config.ts
@@ -4,9 +4,9 @@ import cdkExports from '../cdk-exports';
 const awsConfig = _.cloneDeep(cdkExports);
 
 // Overwrite to local endpoint
-awsConfig.aws_graphqlEndpoint_authNone = 'http://localhost:3100/lambda-gq-resolver/graphql';
-awsConfig.aws_graphqlEndpoint_authRole = 'http://localhost:3100/lambda-gq-resolver/graphql';
-awsConfig.aws_graphqlEndpoint_authUser = 'http://localhost:3100/lambda-gq-resolver/graphql';
+// awsConfig.aws_graphqlEndpoint_authNone = 'http://localhost:3100/lambda-gq-resolver/graphql';
+// awsConfig.aws_graphqlEndpoint_authRole = 'http://localhost:3100/lambda-gq-resolver/graphql';
+// awsConfig.aws_graphqlEndpoint_authUser = 'http://localhost:3100/lambda-gq-resolver/graphql';
 
 awsConfig.oauth.redirectSignIn = `${window.location.protocol}//${window.location.host}${cdkExports.oauth.redirectSignIn}`;
 awsConfig.oauth.redirectSignOut = `${window.location.protocol}//${window.location.host}${cdkExports.oauth.redirectSignIn}`;
