da0adfc1c1c5073eb9071b9f2153934572cfe886
Simon <simonaverhoeven@gmail.com>
2020-12-30 18:52:44 +0800

    commit after pipeline redeploy

--- packages/cdk-app/bin/packages.ts
+++ packages/cdk-app/bin/packages.ts
@@ -13,7 +13,7 @@ import { CdkpipelinesDemoPipelineStack } from '../lib/cdk-pipelines-demo-pipelin
 module.exports = {};
 
 const app = new cdk.App();
-new CdkpipelinesDemoPipelineStack(app, 'CDK-MonoRepo-Frontend-Pipeline-2', {
+new CdkpipelinesDemoPipelineStack(app, 'CDK-MonoRepo-Frontend-Pipeline', {
   env: {
     account: '694710432912',
     region: 'ap-southeast-1',
