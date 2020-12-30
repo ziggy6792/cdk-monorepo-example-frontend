c9646eb3aaeb243e413cee0593b07e9e226bab22
Simon <simonaverhoeven@gmail.com>
2020-12-30 18:35:16 +0800

    changed synth command

--- packages/cdk-app/lib/cdk-pipelines-demo-pipeline-stack.ts
+++ packages/cdk-app/lib/cdk-pipelines-demo-pipeline-stack.ts
@@ -41,6 +41,7 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
         // Use this if you need a build step (if you're not using ts-node
         // or if you have TypeScript Lambdas that need to be compiled).
         buildCommand: 'yarn run build',
+        synthCommand: 'yarn cdk-synth',
       }),
     });
 
