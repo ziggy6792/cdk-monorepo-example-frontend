3a00d39c7823e119bf10f2c10345e047010d60d6
Simon <simonaverhoeven@gmail.com>
2021-01-04 12:20:25 +0800

    rename cdk-synth

--- packages/cdk-app/lib/pipeline-stack.ts
+++ packages/cdk-app/lib/pipeline-stack.ts
@@ -42,7 +42,7 @@ class PipelineStack extends Stack {
         // Use this if you need a build step (if you're not using ts-node
         // or if you have TypeScript Lambdas that need to be compiled).
         buildCommand: 'yarn run build',
-        synthCommand: 'yarn cdk-synth',
+        synthCommand: 'yarn cdk:synth',
         // subdirectory: 'packages/cdk-app',
       }),
     });
