b6ab1737d10a500bdc8a1117ca3c0097ad786048
Simon <simonaverhoeven@gmail.com>
2021-01-19 01:01:18 +0800

    set test env

--- packages/web-app/jest.config.js
+++ packages/web-app/jest.config.js
@@ -12,5 +12,10 @@ module.exports = {
     displayName: package.name,
     roots: ['<rootDir>/src'],
     moduleDirectories: ['node_modules', '<rootDir>'],
-    moduleNameMapper,
+    moduleNameMapper: {
+        ...moduleNameMapper,
+        '\\.(css|scss)$': 'identity-obj-proxy',
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/_mocks_/fileMock.js',
+    },
+    testEnvironment: 'jsdom',
 };
--- packages/web-app/src/screens/profile-screen/profile-screen.test.tsx
+++ packages/web-app/src/screens/profile-screen/profile-screen.test.tsx
@@ -1,8 +1,12 @@
 /* eslint-disable react/button-has-type */
+import { render } from '@testing-library/react';
 import React, { useState } from 'react';
+import ProfileScreen from './profile-screen';
 
 describe('Profile Screen Test', () => {
     it('should be good', () => {
+        render(<ProfileScreen />);
+
         expect(1 + 1 === 2);
     });
 });
