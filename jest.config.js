112c3793575a1e7404d1136aff28e503a8df3cc7
Simon <simonaverhoeven@gmail.com>
2021-01-19 04:11:53 +0800

    cleanup tests

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,5 @@
 const base = require('./jest.config.base.js');
 
 module.exports = {
-    ...base,
     projects: ['<rootDir>/packages/*/jest.config.js'],
-    // projects: ['<rootDir>/jest.config.js'],
 };
