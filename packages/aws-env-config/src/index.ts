6c5d1c6b70bffd98a2bb9128b53a7d85d68a6889
Simon <simonaverhoeven@gmail.com>
2021-01-20 04:57:36 +0800

    log caller

--- packages/aws-env-config/src/index.ts
+++ packages/aws-env-config/src/index.ts
@@ -1,9 +1,22 @@
 #!/usr/bin/env node
 
 import AWS from 'aws-sdk';
+import util from 'util';
 import fetchConfig from './fetch-config';
 
 AWS.config.update({ region: 'ap-southeast-1' });
+
+console.log('HELLO');
+console.log(`dir ${__dirname}`);
+
+process.argv.forEach((val, index, array) => {
+    console.log(`${index}: ${val}`);
+});
+
+// util.inspect(process);
+
+console.log('process');
+console.log(`called from ${process.env.INIT_CWD}`);
 
 fetchConfig('/cdk-monorepo-backend/staging/frontend-config')
     .then((response) => {
