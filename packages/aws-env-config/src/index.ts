12905baa4eb241e077704179676e623459bdcda0
Simon <simonaverhoeven@gmail.com>
2021-01-20 04:38:21 +0800

    added aws config package

---
+++ packages/aws-env-config/src/index.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import AWS from 'aws-sdk';
+import fetchConfig from './fetch-config';
+
+AWS.config.update({ region: 'ap-southeast-1' });
+
+fetchConfig('/cdk-monorepo-backend/staging/frontend-config')
+    .then((response) => {
+        console.log(response);
+    })
+    .catch((err) => console.log(err));
+
+// console.log('aws-env-config bla');
