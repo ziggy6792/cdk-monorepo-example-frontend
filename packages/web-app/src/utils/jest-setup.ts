c45a49303490bedc62acc8012b9597e9ea58afb9
Simon <simonaverhoeven@gmail.com>
2021-01-22 01:53:05 +0800

    cleanup

--- packages/web-app/src/utils/jest-setup.ts
+++ packages/web-app/src/utils/jest-setup.ts
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import '@testing-library/jest-dom/extend-expect';
+import 'jest-canvas-mock';
 
 export default {};
 
--- setup-tests.ts
+++
@@ -1,3 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import '@testing-library/jest-dom/extend-expect';
-import 'jest-canvas-mock';
