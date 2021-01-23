d29483bf0c218b21c2317fa1e73208a222f2fdb5
Simon <simonaverhoeven@gmail.com>
2021-01-23 19:04:42 +0800

    added region and account

--- packages/cdk-app/src/frontend-stack/pipeline-stack.ts
+++ packages/cdk-app/src/frontend-stack/pipeline-stack.ts
@@ -46,7 +46,7 @@ class PipelineStack extends cdk.Stack {
                 synthCommand: 'yarn cdk:synth',
                 rolePolicyStatements: [
                     new iam.PolicyStatement({
-                        resources: [`arn:aws:ssm:::parameter/cdk-monorepo-*`],
+                        resources: [`arn:aws:ssm:${config.AWS_REGION}:${config.AWS_ACCOUNT_ID}:parameter/cdk-monorepo-*`],
                         actions: ['ssm:GetParameter'],
                         effect: iam.Effect.ALLOW,
                     }),
