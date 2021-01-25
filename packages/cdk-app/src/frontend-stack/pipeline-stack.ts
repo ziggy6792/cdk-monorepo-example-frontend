5e7f7b4c24c7fe912406ef8862a60ffab094ab02
Simon <simonaverhoeven@gmail.com>
2021-01-25 12:56:22 +0800

    switced pipeline

--- packages/cdk-app/src/frontend-stack/pipeline-stack.ts
+++ packages/cdk-app/src/frontend-stack/pipeline-stack.ts
@@ -33,7 +33,7 @@ class PipelineStack extends cdk.Stack {
                 // Replace these with your actual GitHub project info
                 owner: 'ziggy6792',
                 repo: 'cdk-monorepo-example-frontend',
-                branch: 'master',
+                branch: 'migrate/new-account',
             }),
 
             synthAction: cdkPipeline.SimpleSynthAction.standardYarnSynth({
