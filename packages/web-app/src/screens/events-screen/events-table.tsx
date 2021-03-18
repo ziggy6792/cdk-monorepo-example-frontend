28a3120a18dabd0df6c023cb92f30e5026c9c092
simon-verhoeven <simonaverhoeven@gmail.com>
2021-03-18 16:42:43 +0800

    added events table

--- packages/web-app/src/screens/events-screen/events-table.tsx
+++ packages/web-app/src/screens/events-screen/events-table.tsx
@@ -1,8 +1,13 @@
 /* eslint-disable no-underscore-dangle */
+
 import React from 'react';
-import { useQuery } from 'react-apollo';
+import MUIDataTable from 'mui-datatables';
+
+import _ from 'lodash';
+import { Button, Grid } from '@material-ui/core';
+
 import Event from 'src/domain/models/event';
-import { LIST_EVENTS } from 'src/graphql/custom-queries';
+import { useHistory } from 'react-router';
 
 interface EventsTableProps {
     events: Event[];
@@ -10,9 +15,53 @@ interface EventsTableProps {
 
 const EventsTable: React.FC<EventsTableProps> = ({ events }) => {
     console.log(events);
+
+    const history = useHistory();
+
+    const tableData = events && events.map((val) => [val.name, '06/07/1992', { rowMeta: { id: val.id } }]);
+
+    const columns = [
+        {
+            name: 'event',
+            label: 'Event',
+            options: {},
+        },
+        {
+            name: 'date',
+            label: 'Date',
+            options: {},
+        },
+    ];
+
     return (
         <>
-            <div>{JSON.stringify(events)}</div>
+            <Grid container direction='column'>
+                <Grid item>
+                    <MUIDataTable
+                        title='Events'
+                        data={tableData}
+                        columns={columns}
+                        options={{
+                            onRowClick: (rowData) => {
+                                console.log(`clicked`, rowData);
+                            },
+                        }}
+                    />
+                </Grid>
+                {/* {props.auth.isAuthenticated && (
+                    <Grid container direction='row' justify='center' style={{ marginTop: theme.spacing(2) }}>
+                        <Grid item>
+                            <Button
+                                onClick={() => {
+                                    setOpen(true);
+                                }}
+                            >
+                                Create Event
+                            </Button>
+                        </Grid>
+                    </Grid>
+                )} */}
+            </Grid>
         </>
     );
 };
