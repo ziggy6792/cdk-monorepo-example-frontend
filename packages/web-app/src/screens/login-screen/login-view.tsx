6422ad77277c40141abb5320702aa25dccd8f9df
Renate Gouveia <renate.gouveia@gmail.com>
2021-03-21 01:02:58 +0800

    Clean up UI login

--- packages/web-app/src/screens/login-screen/login-view.tsx
+++ packages/web-app/src/screens/login-screen/login-view.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Card from 'src/components/atoms/card';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
 import Spinner from '@material-ui/core/CircularProgress';
 import Button from '@material-ui/core/Button';
@@ -36,60 +37,67 @@ const LoginView: React.FC<IProps> = ({ onSubmit, error, loading }) => {
     return (
         <Grid container direction='column' justify='center' alignItems='center' spacing={3} style={{ height: '100%' }}>
             <Grid item>
-                <Typography variant='h2'>Login</Typography>
+                <Typography align='center' variant='h3'>
+                    Welcome Back
+                </Typography>
+                <Typography color='secondary' variant='h5'>
+                    Don&apos;t miss out the latest events, Sing in to stay updated!
+                </Typography>
             </Grid>
             <Grid item>
-                <Card>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <Grid container spacing={3}>
-                            <Grid item xs={12}>
-                                <Grid container spacing={2}>
-                                    <Grid item xs={12}>
-                                        <TextField
-                                            fullWidth
-                                            label='Email'
-                                            name='email'
-                                            size='small'
-                                            variant='outlined'
-                                            inputRef={register(validations.email)}
-                                        />
-                                        {errors.email && (
-                                            <Typography color='error' variant='body2' style={{ fontSize: 12 }}>
-                                                {errors.email.message}
-                                            </Typography>
-                                        )}
-                                    </Grid>
-                                    <Grid item xs={12}>
-                                        <TextField
-                                            fullWidth
-                                            label='Password'
-                                            name='password'
-                                            size='small'
-                                            type='password'
-                                            variant='outlined'
-                                            inputRef={register(validations.password)}
-                                        />
-                                        {errors.password && (
-                                            <Typography color='error' variant='body2' style={{ fontSize: 12 }}>
-                                                {errors.password.message}
-                                            </Typography>
-                                        )}
+                <Container maxWidth='sm'>
+                    <Card>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <Grid container spacing={3}>
+                                <Grid item xs={12}>
+                                    <Grid container spacing={2}>
+                                        <Grid item xs={12}>
+                                            <TextField
+                                                fullWidth
+                                                label='Email'
+                                                name='email'
+                                                size='small'
+                                                variant='outlined'
+                                                inputRef={register(validations.email)}
+                                            />
+                                            {errors.email && (
+                                                <Typography color='error' variant='body2' style={{ fontSize: 12 }}>
+                                                    {errors.email.message}
+                                                </Typography>
+                                            )}
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <TextField
+                                                fullWidth
+                                                label='Password'
+                                                name='password'
+                                                size='small'
+                                                type='password'
+                                                variant='outlined'
+                                                inputRef={register(validations.password)}
+                                            />
+                                            {errors.password && (
+                                                <Typography color='error' variant='body2' style={{ fontSize: 12 }}>
+                                                    {errors.password.message}
+                                                </Typography>
+                                            )}
+                                        </Grid>
                                     </Grid>
                                 </Grid>
+                                <Grid item xs={12}>
+                                    <Button color='primary' fullWidth type='submit' variant='contained'>
+                                        {loading ? <Spinner /> : 'Login'}
+                                    </Button>
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <Typography color='error' variant='body2' style={{ fontSize: 12 }}>
+                                        {error}
+                                    </Typography>
+                                </Grid>
                             </Grid>
-                            <Grid item xs={12}>
-                                <Button color='primary' fullWidth type='submit' variant='contained'>
-                                    {loading ? <Spinner /> : 'Login'}
-                                </Button>
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Typography color='error' variant='body2' style={{ fontSize: 12 }}>
-                                    {error}
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                    </form>
-                </Card>
+                        </form>
+                    </Card>
+                </Container>
             </Grid>
         </Grid>
     );
