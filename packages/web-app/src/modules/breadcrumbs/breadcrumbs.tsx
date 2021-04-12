761bdff7cb3c5e57501cc5d6152a83ddee84cbaf
simon-verhoeven <simonaverhoeven@gmail.com>
2021-04-12 20:43:56 +0800

    added ui

---
+++ packages/web-app/src/modules/breadcrumbs/breadcrumbs.tsx
@@ -0,0 +1,30 @@
+import { Breadcrumbs as MuiBreadcrumbs, Link, Typography } from '@material-ui/core';
+import React from 'react';
+import * as routes from 'src/config/routes';
+import { LinkList, LinkType } from 'src/generated-types';
+
+interface IBreadcrumbs {
+    breadcrumbs: LinkList;
+}
+
+const routeLookup = {
+    [LinkType.Event]: routes.ROUTE_EVENT,
+    [LinkType.Competition]: routes.ROUTE_COMPETITION,
+    [LinkType.Heat]: routes.ROUTE_HEAT,
+};
+
+const Breadcrumbs: React.FC<IBreadcrumbs> = ({ breadcrumbs }) => (
+    <MuiBreadcrumbs aria-label='breadcrumb'>
+        {breadcrumbs.items.map(({ name, type, id }, i) =>
+            i < breadcrumbs.items.length - 1 ? (
+                <Link color='inherit' href={`${routeLookup[type]}/${id}`}>
+                    {name}
+                </Link>
+            ) : (
+                <Typography color='textPrimary'>{name}</Typography>
+            )
+        )}
+    </MuiBreadcrumbs>
+);
+
+export default Breadcrumbs;
--- packages/web-app/src/screens/competition-screen/competition-screen.tsx
+++ packages/web-app/src/screens/competition-screen/competition-screen.tsx
@@ -6,6 +6,7 @@ import Spinner from 'src/components/spinner';
 import CompetitionSummary from 'src/modules/summary/competition-summary';
 import { ROUTE_COMPETITION_MANAGER } from 'src/config/routes';
 import { useHistory } from 'react-router';
+import Breadcrumbs from 'src/modules/breadcrumbs/breadcrumbs';
 import HeatsStructure from './heats-structure/heats-structure';
 
 interface IEventsScreenProps {
@@ -15,37 +16,36 @@ interface IEventsScreenProps {
 const CompetitionScreen: React.FC<IEventsScreenProps> = ({ competitionId }) => {
     const { loading, data } = useGetCompetitionQuery({ variables: { id: competitionId } });
 
-    // const heats = !loading ? _.flatten(data.getCompetition.rounds.items.map((round) => round.heats.items)) : [];
-    // const riderAllocations = !loading ? data.getCompetition.riderAllocations.items : [];
-
-    // const disabledRiderAllocationButtons = data?.getCompetition.riderAllocations.items.length < 1;
-
     const history = useHistory();
 
     return (
         <>
             {loading && <Spinner />}
             {!loading && (
-                <Grid container direction='column' justify='center' alignItems='center'>
-                    <Grid container direction='row' justify='flex-end' alignItems='center'>
-                        <Grid item>
-                            <Button
-                                onClick={() => {
-                                    history.push(`${ROUTE_COMPETITION_MANAGER}/${competitionId}`);
-                                }}
-                            >
-                                Open Competition Manager
-                            </Button>
+                <>
+                    <Breadcrumbs breadcrumbs={data.getCompetition.breadcrumbs} />
+                    <Grid container direction='column' justify='center' alignItems='center'>
+                        <Grid container direction='row' justify='flex-end' alignItems='center'>
+                            <Grid item>
+                                <Button
+                                    onClick={() => {
+                                        history.push(`${ROUTE_COMPETITION_MANAGER}/${competitionId}`);
+                                    }}
+                                >
+                                    Open Competition Manager
+                                </Button>
+                            </Grid>
+                        </Grid>
+                        {/* ToDo: Fix this */}
+                        <Grid item style={{ width: '100%' }}>
+                            <CompetitionSummary summary={data.getCompetition} />
+                        </Grid>
+                        <Grid item style={{ width: '95%' }}>
+                            <HeatsStructure rounds={data.getCompetition.rounds.items} eventId={data.getCompetition.event.id} />
                         </Grid>
                     </Grid>
-                    {/* ToDo: Fix this */}
-                    <Grid item style={{ width: '100%' }}>
-                        <CompetitionSummary summary={data.getCompetition} />
-                    </Grid>
-                    <Grid item style={{ width: '95%' }}>
-                        <HeatsStructure rounds={data.getCompetition.rounds.items} eventId={data.getCompetition.event.id} />
-                    </Grid>
-                </Grid>
+                    )
+                </>
             )}
         </>
     );
