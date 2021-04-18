ac20afd4fadf4d75ac34fc2b64d11a8563f31e53
simon-verhoeven <simonaverhoeven@gmail.com>
2021-04-19 03:05:25 +0800

    fix nav

--- packages/web-app/src/components/ui/bottom-navigation.tsx
+++ packages/web-app/src/components/ui/bottom-navigation.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import { useHistory } from 'react-router';
 
-import { 
-  createStyles, 
-  makeStyles, 
-  Theme, 
-  Card, 
-  BottomNavigation as MuiBottomNavigation, 
-  BottomNavigationAction 
-} from '@material-ui/core';
+import { createStyles, makeStyles, Theme, Card, BottomNavigation as MuiBottomNavigation, BottomNavigationAction } from '@material-ui/core';
 import { EventNote, AccountTree, Home, AccountCircle, ControlCamera } from '@material-ui/icons';
 import { ROUTE_LIVE, ROUTE_TIMETABLE, ROUTE_PROFILE, ROUTE_EVENT, ROUTE_TOURNAMENT } from 'src/config/routes';
 
@@ -44,14 +37,14 @@ interface BottomNavProps {
 const BottomNavigation: React.FC<BottomNavProps> = ({ eventId, currentPath }) => {
   const classes = useStyles();
   const history = useHistory();
-  
+
   const routes = {
-    "tournament": ROUTE_TOURNAMENT,
-    "timetable": ROUTE_TIMETABLE,
-    "overall": ROUTE_EVENT,
-    "live": ROUTE_LIVE,
-    "profile": ROUTE_PROFILE,
-  }
+    tournament: ROUTE_TOURNAMENT,
+    timetable: ROUTE_TIMETABLE,
+    overall: ROUTE_EVENT,
+    live: ROUTE_LIVE,
+    profile: ROUTE_PROFILE,
+  };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>, newValue: string) => {
     history.push(`${routes[newValue]}/${eventId}`);
@@ -59,15 +52,15 @@ const BottomNavigation: React.FC<BottomNavProps> = ({ eventId, currentPath }) =>
 
   return (
     <Card className={classes.root}>
-      <MuiBottomNavigation value={currentPath} showLabels onChange={handleChange} className={classes.root}>
+      <MuiBottomNavigation value={currentPath} showLabels onChange={handleChange}>
         {/* <BottomNavigationAction label="Tournament" value="tournament" icon={<AccountTree style={{ transform: "scaleX(-1)" }} />} /> */}
-        <BottomNavigationAction label="Overall" value="overall" icon={<Home />} />
-        <BottomNavigationAction label="Timetable" value="timetable" icon={<EventNote />} />
-        <BottomNavigationAction label="Live" value="live" icon={<ControlCamera />} />
-        <BottomNavigationAction label="Admin" value="profile" icon={<AccountCircle />} />
-      </MuiBottomNavigation>      
+        <BottomNavigationAction label='Overall' value='overall' icon={<Home />} />
+        <BottomNavigationAction label='Timetable' value='timetable' icon={<EventNote />} />
+        <BottomNavigationAction label='Live' value='live' icon={<ControlCamera />} />
+        <BottomNavigationAction label='Admin' value='profile' icon={<AccountCircle />} />
+      </MuiBottomNavigation>
     </Card>
-  )
+  );
 };
 
 export default BottomNavigation;
--- packages/web-app/src/components/ui/screen-wrapper.tsx
+++ packages/web-app/src/components/ui/screen-wrapper.tsx
@@ -3,19 +3,19 @@ import clsx from 'clsx';
 
 import { createStyles, makeStyles, Theme } from '@material-ui/core';
 import TopNavigation from './top-navigation';
-import BottomNavigation from './bottom-navigation'
+import BottomNavigation from './bottom-navigation';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     normalContent: {
-      padding: '100px 0 140px'
+      padding: '100px 0 140px',
     },
     denseContent: {
-      padding: '70px 0 140px'
+      padding: '70px 0 140px',
     },
     onlyBottomContent: {
-      padding: '0 0 140px'
-    }
+      padding: '0 0 140px',
+    },
   })
 );
 
@@ -30,7 +30,7 @@ interface ScreenWrapperProps {
   dateString?: string;
   currentPath: 'tournament' | 'live' | 'overall' | 'timetable' | 'profile';
   dense?: boolean;
-  onlyBottom?: boolean
+  onlyBottom?: boolean;
 }
 
 const ScreenWrapper: React.FC<ScreenWrapperProps> = ({ eventTitle, dateString, eventId, currentPath, dense, onlyBottom = false, children }) => {
@@ -43,13 +43,14 @@ const ScreenWrapper: React.FC<ScreenWrapperProps> = ({ eventTitle, dateString, e
         className={clsx({
           [classes.normalContent]: !dense && !onlyBottom,
           [classes.denseContent]: dense && !onlyBottom,
-          [classes.onlyBottomContent]: onlyBottom
-        })}>
+          [classes.onlyBottomContent]: onlyBottom,
+        })}
+      >
         {children}
       </div>
       <BottomNavigation eventTitle={eventTitle} eventId={eventId} currentPath={currentPath} />
     </>
-  )
+  );
 };
 
 export default ScreenWrapper;
