6c3f03c7f0ddf0889b4222f877cdcae48a3da3d9
Lorenzo Ong <31330139+Lorenzowy@users.noreply.github.com>
2021-04-21 01:29:50 +0800

    responsive to laptop screen

--- packages/web-app/src/components/ui/screen-wrapper.tsx
+++ packages/web-app/src/components/ui/screen-wrapper.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import clsx from 'clsx';
 
-import { createStyles, makeStyles, Theme } from '@material-ui/core';
+import { createStyles, makeStyles, Theme, Grid } from '@material-ui/core';
 import Spinner from 'src/components/spinner';
 import TopNavigation from './top-navigation';
 import BottomNavigation from './bottom-navigation';
@@ -44,16 +44,20 @@ const ScreenWrapper: React.FC<ScreenWrapperProps> = ({ eventTitle, dateString, e
   return (
     <>
       {!onlyBottom && <TopNavigation eventTitle={eventTitle} dateString={dateString} dense={dense} />}
-      <div
-        className={clsx({
-          [classes.normalContent]: !dense && !onlyBottom,
-          [classes.denseContent]: dense && !onlyBottom,
-          [classes.onlyBottomContent]: onlyBottom,
-        })}
-      >
-        {showSpinner && <Spinner />}
-        {!showSpinner && children}
-      </div>
+      <Grid container style={{ placeContent: 'center' }}>
+        <Grid item xs={12} sm={11} md={8} lg={6} xl={4}>
+          <div
+            className={clsx({
+              [classes.normalContent]: !dense && !onlyBottom,
+              [classes.denseContent]: dense && !onlyBottom,
+              [classes.onlyBottomContent]: onlyBottom,
+            })}
+          >
+            {showSpinner && <Spinner />}
+            {!showSpinner && children}
+          </div>
+        </Grid>
+      </Grid>
       <BottomNavigation eventTitle={eventTitle} eventId={eventId} currentPath={currentPath} />
     </>
   );
